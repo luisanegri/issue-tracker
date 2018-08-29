e6cdd392bb679e92dab2e4343f058b78556e3bfd
Luisa Negri <luisaanegri@gmail.com>
2018-08-29 10:31:13 +0000

    downloaded django-forms-bootstrap/updated installed apps/updated templates dirs

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_forms_bootstrap',
     'accounts',
+    
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'issuetracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
