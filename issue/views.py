5a760b369a0def06d8d24478fe76c557c36c3e6b
Luisa Negri <luisaanegri@gmail.com>
2018-08-30 14:13:52 +0000

    created function to get more details and display comments

--- issue/views.py
+++ issue/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from .models import Issue
+from django.shortcuts import render, get_object_or_404
+from .models import Issue, Comment
 
 # Create your views here.
 def all_issues(request):
+    # Get all issues in the database
     issues = Issue.objects.all()
     return render(request, 'issues.html', {'issues': issues})
+    
+def get_detail(request, pk):
+    # Get specific issue
+    issue = get_object_or_404(Issue, pk=pk)
+    # Filters comments in the database of specific issue
+    comment = Comment.objects.filter(issue=pk)
+    return render(request, 'detail.html', {'issue': issue}, {'comment': comment})
+    
