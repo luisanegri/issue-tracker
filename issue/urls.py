d729f5a21d7fe06859f945c940771247fbe82b1c
Luisa Negri <luisaanegri@gmail.com>
2018-08-30 14:16:00 +0000

    updated with get_detail

--- issue/urls.py
+++ issue/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_issues
+from .views import all_issues, get_detail
 
 
 urlpatterns = [
     url(r'^$', all_issues, name='issues'),
+    url(r'^(?P<pk>\d+)/$', get_detail, name='get_detail'),
 ]
