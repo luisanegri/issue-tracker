cfaf012c814ec5667516750910a41fb893f41551
Luisa Negri <luisaanegri@gmail.com>
2018-09-03 13:04:23 +0000

    added more fields

--- issue/models.py
+++ issue/models.py
@@ -3,15 +3,19 @@ from django.contrib.auth.models import User
 
 # Product
 class Issue(models.Model):
-    title = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     description = models.TextField(max_length=200)
-    date_created = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    to_do = models.BooleanField(blank=False, default=True)
+    done = models.BooleanField(blank=False, default=False)
+    doing = models.BooleanField(blank=False, default=False)
     user = models.ForeignKey(User,related_name='user_issue', on_delete=models.CASCADE)
     upvotes = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     
     def __str__(self):
-        return self.title
+        return self.name
 
 # Gives the user the ability to comment on issues
 class Comment(models.Model):
