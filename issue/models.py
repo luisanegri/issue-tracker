7c16d5a6edbfcd9697db880c5fbb4db3e8f37a54
Luisa Negri <luisaanegri@gmail.com>
2018-09-03 17:00:36 +0000

    added more fields

--- issue/models.py
+++ issue/models.py
@@ -1,22 +1,55 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Product
 class Issue(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=200)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    to_do = models.BooleanField(blank=False, default=True)
-    done = models.BooleanField(blank=False, default=False)
-    doing = models.BooleanField(blank=False, default=False)
     user = models.ForeignKey(User,related_name='user_issue', on_delete=models.CASCADE)
     upvotes = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     
     def __str__(self):
         return self.name
+
+    class Varieties(models.Model):
+        BUG = 'Bug'
+        FEATURE = 'Feature'
+  
+        VARIETIES_CHOICES = (
+            (BUG, 'Bug'),
+            (FEATURE, 'Feature'),
+        )
+        varieties = models.CharField(
+            max_length=40,
+            choices=VARIETIES_CHOICES,
+            default=BUG,
+        )
+    
+        def __str__(self):
+            return self.varieties
+
+    class Status(models.Model):
+        TODO = 'To do'
+        DOING = 'Doing'
+        DONE = 'Done'
+  
+        STATUS_CHOICES = (
+            (TODO, 'To do'),
+            (DOING, 'Doing'),
+            (DONE, 'Done'),
+        )
+        status = models.CharField(
+            max_length=40,
+            choices=STATUS_CHOICES,
+            default=TODO,
+        )
+    
+        def __str__(self):
+            return self.status
+
 
 # Gives the user the ability to comment on issues
 class Comment(models.Model):
