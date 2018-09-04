16af2f0ee78d388e7bd96aa03e962debf95f9659
Luisa Negri <luisaanegri@gmail.com>
2018-09-04 15:20:15 +0000

    url for create new issue

--- issue/urls.py
+++ issue/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
-from .views import all_issues, get_detail
+from .views import all_issues, get_detail, create_issue
 
 
 urlpatterns = [
     url(r'^$', all_issues, name='issues'),
+    url(r'^new$', create_issue, name='new_issue'),
     url(r'^(?P<pk>\d+)/$', get_detail, name='get_detail'),
+    
 ]
