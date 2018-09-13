7339e0c1a996db23c37ba23d214a16539e6b0918
Luisa Negri <luisaanegri@gmail.com>
2018-09-13 10:59:53 +0000

    comment form

--- issue/forms.py
+++ issue/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Issue
+from .models import Issue, Comment
 
 class IssueForm(forms.ModelForm):
     class Meta:
         model = Issue
         fields = ('name', 'description', 'varieties')
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment',)
