fb20e8ad7f1060099ba649ec7732062c5c569a87
Luisa Negri <luisaanegri@gmail.com>
2018-09-13 13:53:01 +0000

    created view and url to comment an issue

--- issue/views.py
+++ issue/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Issue, Comment
-from .forms import IssueForm
+from .forms import IssueForm, CommentForm
 
-# Create your views here.
+
 def my_issues(request):
     user = request.user.id
     issues = Issue.objects.filter(created_by=user)
@@ -10,17 +10,17 @@ def my_issues(request):
     
     
 def all_issues(request):
-    # Get all issues in the database
     issues = Issue.objects.all()
     return render(request, 'issues.html', {'issues': issues})
-    
+
+
 def get_detail(request, pk):
-    # Get specific issue
     issue = get_object_or_404(Issue, pk=pk)
-    # Filters comments in the database of specific issue
-    comments = Comment.objects.filter(issue=pk)
+    comments = Comment.objects.all()
+    print(issue.comments)
     return render(request, 'detail.html', {'issue': issue}, {'comments': comments})
-    
+ 
+   
 def create_issue(request):
     if request.method == 'POST':
         form = IssueForm(request.POST)
@@ -30,7 +30,8 @@ def create_issue(request):
     else:
         form = IssueForm()
     return render(request, 'issueform.html', {'form': form})
-    
+
+   
 def edit_issue(request, pk):
     issue = get_object_or_404(Issue, pk=pk)
     if request.method == 'POST':
@@ -41,3 +42,22 @@ def edit_issue(request, pk):
     else:
         form = IssueForm(instance=issue)
     return render(request, 'issueform.html', {'form': form})
+
+    
+def create_comment(request, pk):
+    comments = Comment.objects.all()
+    issue = get_object_or_404(Issue, pk=pk) if pk else None
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.instance.created_by = request.user
+            form.instance.issue = issue
+            form.save()
+            return redirect('get_detail', pk=issue.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'commentform.html', {'form': form, 'comments': comments})
+
+
+
