3577c6d14a2f0003af80aebf84e09746a58860c9
Luisa Negri <luisaanegri@gmail.com>
2018-09-17 11:05:41 +0000

    stripe keys/add checkout app/hide secret key

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@r4ykbb$y5lz8il8qx!u***y0vj*t#xk07lc#ve4)frk+n2a9p'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'issue',
     'cart',
+    'checkout',
     
 ]
 
