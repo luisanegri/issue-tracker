468c31da3c7ff46b48646e1bab7c7852a4581277
Luisa Negri <luisaanegri@gmail.com>
2018-10-09 11:53:20 +0000

    corrected name of the field

--- issue/tests.py
+++ issue/tests.py
@@ -4,6 +4,6 @@ from .models import Issue
 # Create your tests here.
 
 class IssueTests(TestCase):
-    def test_title(self):
-        test_title = Issue(title="A bug")
-        self. assertEqual(str(test_title), "A bug")
+    def test_name(self):
+        test_name = Issue(name="A bug")
+        self. assertEqual(str(test_name), "A bug")
