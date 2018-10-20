f591b629bd68c0420eeb0f607b6aed0880a5db55
Luisa Negri <luisaanegri@gmail.com>
2018-10-20 15:09:00 +0000

    removed logout message success

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from .forms import UserLoginForm, UserRegistrationForm
 from django.template.context_processors import csrf
 from django.contrib.auth.decorators import login_required
+from issue.models import Issue
 
 
 # Create your views here.
@@ -15,7 +16,7 @@ def index(request):
 def logout(request):
     """A view that logs the user out and redirects back to the index page"""
     auth.logout(request)
-    messages.success(request, 'You have successfully logged out')
+
     return redirect(reverse('index'))
 
 
